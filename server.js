17bfa0c746bb21fb4ec7c5dfa6e48888816c0c78
km4891 <kmorarity@gmail.com>
2021-02-06 17:18:55 -0800

    still trying

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const api = require('./routes/api')
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
@@ -23,7 +24,7 @@ mongoose.connect(MONGODB_URI, {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(api);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
